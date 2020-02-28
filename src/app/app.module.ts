6de7feb454c7b1d0970f0fdfb7aaf1abed9dc72c
Jimbo Rountree <james.d.rountree@gmail.com>
2020-02-28 15:53:08 -0500

    add routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,30 @@
 import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+
+const appRoutes: Routes = [
+  {
+    path: '**', redirectTo: 'AppComponent',
+    // pathMatch: 'full'
+  },
+]
+
+
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes
+    ),
   ],
   providers: [],
   bootstrap: [AppComponent]
